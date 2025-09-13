29cda9702cbb58dc58f448133ae64bcabd05f802
Logan Stum <loganstum@aol.com>
2025-09-12 22:10:49 -0400

    trying this

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: 'https://logan-stum.github.io/ProgressMonitor.github.io/',
+  base: '/ProgressMonitor.github.io/',
   plugins: [
     react(),
     VitePWA({
