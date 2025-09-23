f5ce002e08e2287fa230ffdd786800d03509128b
Logan Stum <loganstum@aol.com>
2025-09-22 22:04:16 -0400

    Moved x closer

--- src/App.jsx
+++ src/App.jsx
@@ -217,8 +217,8 @@ function App() {
         tension: 0.3,
         fill: false,
         pointRadius: 5,
-        pointHoverRadius: 20,      // increase hover radius
-        pointHitRadius: 20,        // increase clickable area
+        pointHoverRadius: 50,      // increase hover radius
+        pointHitRadius: 50,        // increase clickable area
       },
       activeChart?.startDate &&
         activeChart?.goalDate && {
