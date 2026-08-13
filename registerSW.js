1fa2bf4b5c978a4370e68c957ac3d46c36c09030
Logan Stum <loganstum@aol.com>
2026-08-13 16:54:39 -0400

    Updates

--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ProgressMonitor.github.io/sw.js', { scope: '/ProgressMonitor.github.io/' })})}
